24d7637fa31ff4bd13ff83066bcfc5cc60cee2d0
Anthony Ramos <ramosanthony@gmail.com>
2023-06-01 11:37:07 -0400

    Part 2

---
+++ part-2/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div>
+      <Tweet
+        name="Juri Han"
+        username="Jurinohans"
+        date={new Date().toDateString()}
+        message="I did it."
+      />
+      <Tweet
+        name="Ken Masters"
+        username="FlameMaser"
+        date={new Date().toDateString()}
+        message="It's warm in here."
+      />
+      <Tweet
+        name="Luke Sullivan"
+        username="FiteNite"
+        date={new Date().toDateString()}
+        message="Don't talk about Fite Nite."
+      />
+    </div>
+  );
+}
